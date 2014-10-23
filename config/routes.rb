a8a0e02f0f784198f7553e34bc06594699e1afcd
Julian Russell <jr.plusplus@gmail.com>
2014-10-23 11:48:39 +1100

    Added route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :donations, only: %w(new create show)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
