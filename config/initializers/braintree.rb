cf5272fb864a2db6badf35fa3b013cfbed1c3b4e
Julian Russell <jr.plusplus@gmail.com>
2014-10-23 12:24:29 +1100

    Added initializer (would be better in ENV variables)

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = 'kmgzzw34tpjzt3f8'
+Braintree::Configuration.public_key = 'tfp3tvg3dvqpdfyw'
+Braintree::Configuration.private_key = '18f7c7cae9e818b7d83bf2007de03a06'
