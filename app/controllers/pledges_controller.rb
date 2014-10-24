23ea0c3eb2da0d710edce174858118569ccb3d5c
Julian Russell <jr.plusplus@gmail.com>
2014-10-24 23:59:22 +1100

    Added validation

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,26 +1,29 @@
 class PledgesController < ApplicationController
 
   def new
-    @client_token = Braintree::ClientToken.generate
-    @pledge       = Pledge.new
+    prepare_for_form
+    @pledge = Pledge.new
   end
 
   def create
-    pledge = Pledge.new(pledge_params)
-    if pledge.save
+    @pledge = Pledge.new(pledge_params)
+    if @pledge.save
       result = Braintree::Transaction.sale(
-        :amount => "#{pledge.amount}.00",
+        :amount => "#{@pledge.amount}.00",
         :payment_method_nonce => params[:payment_method_nonce]
       )
       if result.success?
-        pledge.transaction_reference = result.transaction.id
-        pledge.save
-        redirect_to(pledge_path(pledge))
+        @pledge.transaction_reference = result.transaction.id
+        @pledge.save
+        redirect_to(pledge_path(@pledge))
       else
-        @client_token = Braintree::ClientToken.generate
         flash.now[:warning] = result.transaction.status
+        prepare_for_form
         render :new
       end
+    else
+      prepare_for_form
+      render :new
     end
   end
 
@@ -29,6 +32,10 @@ class PledgesController < ApplicationController
   end
 
   private
+
+  def prepare_for_form
+    @client_token = Braintree::ClientToken.generate
+  end
 
   def pledge_params
     params.require(:pledge).permit(:name, :amount)
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,2 +1,4 @@
 class Pledge < ActiveRecord::Base
+  validates :amount, numericality: true, presence: true
+  validates :name, presence: true
 end
